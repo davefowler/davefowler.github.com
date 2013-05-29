0218b65fa0181d8920e184d5f8cead60c1efe853
dave <davefowler@gmail.com>
2013-05-29 00:47:48 -0700

    None

--- media/js/pin.js
+++ media/js/pin.js
@@ -2,16 +2,14 @@
 
 function embedpin() {
   var youtubelink = jQuery('object param[name="movie"]').val();
-  var youtubecode = youtubelink.match(/\/v\/(.*)\?/)[1];
+  // OLD EMBED CODE var youtubecode = youtubelink.match(/\/v\/(.*)\?/)[1];
+  var youtubecode = youtubelink.match(/\/embed\/(.*)/)[1];
   var thumbnail = 'http://img.youtube.com/vi/' + youtubecode + '/0.jpg';
-  var button = '<a class="pinbutton" href="//pinterest.com/pin/create/button/?url='+escape(window.location.href)+'&media=' + escape(thumbnail) + '&description=' + escape(document.title) + '" data-pin-do="buttonPin" data-pin-config="above"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>'
-
-  
+  var button = '<a class="pinbutton" href="//pinterest.com/pin/create/button/?url='+escape(window.location.href)+'&media=' + escape(thumbnail) + '&description=' + escape(document.title) + '" data-pin-do="buttonPin" data-pin-config="above"><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>';  
   jQuery('.pinbutton').remove();
   jQuery('object').append(
     '<p>'+ button + '</p>'
-  )
-
+  );
 };
 
 jQuery(document).ready(function() {
